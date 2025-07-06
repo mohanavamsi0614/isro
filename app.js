54679f13192c846724d0498400b89cea8166bd02
Mohana Vamsi <mohanavamsi14@gmail.com>
2025-07-06 17:50:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const openai = new OpenAI({
 });
 
 // Middleware
-app.use(cors({orgin:"*"}));
+app.use(cors({origin:"*"}));
 app.use(express.json());
 app.use('/public', express.static(path.join(__dirname, 'public'))); 
 
